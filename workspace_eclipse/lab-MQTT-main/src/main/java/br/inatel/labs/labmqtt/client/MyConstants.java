91f8f6c852940295098612cdd82ca160aa5d149a
Iago Luiz Da Cruz Barbosa <ilbarbosa1@latam.stefanini.com>
2023-12-13 12:42:44 -0300

    :recycle: refactor: Redoing MQTT lab

--- workspace_eclipse/lab-MQTT-main/src/main/java/br/inatel/labs/labmqtt/client/MyConstants.java
+++ workspace_eclipse/lab-MQTT-main/src/main/java/br/inatel/labs/labmqtt/client/MyConstants.java
@@ -2,5 +2,5 @@ package br.inatel.labs.labmqtt.client;
 
 public interface MyConstants {
 	String URI_BROKER = "tcp://localhost:1883";
-	String TOPIC_1 = "meu_topico";
+	String TOPIC_SENSOR = "sensor";
 }
--- workspace_eclipse/lab-MQTT-main/src/main/java/br/inatel/labs/labmqtt/client/SensorTemperaturaPublisher.java
+++ workspace_eclipse/lab-MQTT-main/src/main/java/br/inatel/labs/labmqtt/client/SensorTemperaturaPublisher.java
@@ -10,32 +10,45 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class SensorTemperaturaPublisher {
-	
-	public static void main(String[] args) throws MqttException {
-		//1. criar o publisher
-		String publisherId = UUID.randomUUID().toString();
-		IMqttClient publisher = new MqttClient(MyConstants.URI_BROKER, publisherId);
-		
-		//2. cria a mensagem
-		MqttMessage msg = getTemperaturaSolo();
-		msg.setQos(0);
-		msg.setRetained(true);
-		
-		//3. conecta
-		MqttConnectOptions options = new MqttConnectOptions();
-		options.setAutomaticReconnect(true);
-		options.setCleanSession(true);
-		options.setConnectionTimeout(10);
-		publisher.connect(options);
-		
-		//4. publica
-		publisher.publish(MyConstants.TOPIC_1, msg);
-		
-		//5. desconecta
-		publisher.disconnect();
+
+	public static void main(String[] args) {
+		IMqttClient publisher = null;
+		try {
+			// 1. criar o publisher
+			String publisherId = UUID.randomUUID().toString();
+			publisher = new MqttClient(MyConstants.URI_BROKER, publisherId);
+
+			// 3. conecta
+			MqttConnectOptions options = new MqttConnectOptions();
+			options.setAutomaticReconnect(true);
+			options.setCleanSession(true);
+			options.setConnectionTimeout(10);
+			publisher.connect(options);
+
+			for (int i = 0; i < 10; i++) {
+				// 2. cria a mensagem
+				MqttMessage msg = getTemperatureMessage();
+				msg.setQos(0);
+				msg.setRetained(true);
+
+				// 4. publica
+				publisher.publish(MyConstants.TOPIC_SENSOR, msg);
+				Thread.sleep(2000);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 5. desconecta
+				publisher.disconnect();
+				publisher.close();
+			} catch (MqttException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
-	private static MqttMessage getTemperaturaSolo() {
+	private static MqttMessage getTemperatureMessage() {
 		Random r = new Random();
 		double temperatura = 80 + r.nextDouble() * 20.0;
 		byte[] payload = String.format("T:%04.2f", temperatura).getBytes();
